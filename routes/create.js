1eade4e99b993a509268bcbf6aff12fcaa4de3fd
Kongpon Charanwattanakit <jackykongpon@gmail.com>
2017-08-15 00:25:15 +0700

    add create page

--- routes/create.js
+++ routes/create.js
@@ -3,6 +3,10 @@ import game from '../game'
 const router = express.Router()
 
 /* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('create', { title: 'Create New Game' })
+})
+
 router.post('/', function(req, res, next) {
   const words = req.body.words
   game.create(words)
